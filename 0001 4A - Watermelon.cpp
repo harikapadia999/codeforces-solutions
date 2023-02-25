66320602d3e404255e2777c50038d91bffc6d739
harikapadia999 <99083489+harikapadia999@users.noreply.github.com>
2023-02-25 17:51:22 +0530

    Update 0001 4A - Watermelon.cpp

--- 0001 4A - Watermelon.cpp
+++ 0001 4A - Watermelon.cpp
@@ -10,3 +10,12 @@ int main()
         cout << "NO" << endl;
     return 0;
 }
+/*
+We start by reading the input integer w from the user using the cin function.
+
+Next, we check if the weight of the watermelon is even and greater than 2. If it is, then we print "YES", which means that the boys can divide the watermelon into two parts, each of them weighing even number of kilos. Otherwise, we print "NO", which means that the boys cannot divide the watermelon in the way they want.
+
+The condition w % 2 == 0 checks if the weight of the watermelon is even, and the condition w > 2 checks if the weight is greater than 2. If both conditions are true, we print "YES". Otherwise, we print "NO".
+
+Note that we need to make sure that each part has a positive weight, so we cannot divide the watermelon into two parts of 1 and w - 1 kilos.
+*/
